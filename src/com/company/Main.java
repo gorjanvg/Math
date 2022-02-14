59fa1573b388800b490e84c97225d99a6214140c
gorjan <gorjan_ugrinovsi@yahoo.com>
2022-02-14 17:01:19 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        long a = 35;
+        long b = 30;
+        System.out.println(Math.addExact(a,b));
+    }
+}
